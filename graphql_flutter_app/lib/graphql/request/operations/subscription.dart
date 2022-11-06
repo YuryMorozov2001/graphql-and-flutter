9d9ec07d7ddad5d41104b19f2614c16e2d4988f1
Yury Morozov <morozovypa1@gmail.com>
2022-11-06 18:44:15 +0800

    updated UserSubscription class

--- graphql_flutter_app/lib/graphql/request/operations/subscription.dart
+++ graphql_flutter_app/lib/graphql/request/operations/subscription.dart
@@ -4,4 +4,7 @@ class UserSubscription {
   get() {
     return gql("subscription SubUsers {user {id, name, age }}");
   }
+  getWithTask(){
+    return gql("subscription SubUsers {user {id, name, age, todos {task}}}");
+  }
 }
