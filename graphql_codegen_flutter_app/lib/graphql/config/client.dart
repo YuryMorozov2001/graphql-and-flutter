d3923e2e10754d8b101167d1e838b7440e8ed208
Yury Morozov <morozovypa1@gmail.com>
2022-11-14 21:24:12 +0800

    fixed client

--- graphql_codegen_flutter_app/lib/graphql/config/client.dart
+++ graphql_codegen_flutter_app/lib/graphql/config/client.dart
@@ -1,17 +1,14 @@
-import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
 class GQLClient {
   static HttpLink httpLink = HttpLink('http://localhost:8080/v1/graphql');
   static WebSocketLink webSocketLink =
       WebSocketLink('ws://localhost:8080/v1/graphql');
-  // here may be graphql token 
+  // here may be graphql token
   static Link link =
       Link.split((request) => request.isSubscription, webSocketLink, httpLink);
-  static ValueNotifier<GraphQLClient> client = ValueNotifier(
-    GraphQLClient(
-      link: link,
-      cache: GraphQLCache(store: HiveStore()),
-    ),
+  static final client = GraphQLClient(
+    link: link,
+    cache: GraphQLCache(store: HiveStore()),
   );
 }
